031e6cfe9e8d8118c9f17eae570908175e38fcd9
Sampo Juustila <sampo.juustila@iki.fi>
2019-11-02 22:08:28 +0200

    chore: update test

--- e2e/detox_output.spec.js
+++ e2e/detox_output.spec.js
@@ -10,7 +10,16 @@ describe('Detox output', () => {
       await expectDetox(element(by.id('heading'))).toHaveText('_NOT_PRESENT_');
     } catch (e) {
       const msg = e.message.toString();
-      console.log('OUTPUT: ' + msg);
+      // console.log('OUTPUT FOUND: ' + msg);
+    }
+  });
+
+  it('should log the output for not found element', async () => {
+    try {
+      await expectDetox(element(by.id('foobar'))).toHaveText('_NOT_PRESENT_');
+    } catch (e) {
+      const msg = e.message.toString();
+      //console.log('OUTPUT NOT FOUND: ' + msg);
     }
   });
 });
--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -1,11 +1,13 @@
 const {device, expect: expectDetox, element, by, waitFor} = require('detox');
+const {getText, getProps} = require('detox-getprops');
 
 describe('Example', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
-  it('should work', async () => {
-    await expectDetox(element(by.id('heading'))).toBeVisible();
+  it('should fetch element text', async () => {
+    const text = await getText(element(by.id('heading')));
+    expect(text).toEqual('Step One');
   });
 });
